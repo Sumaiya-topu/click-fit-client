52645fc58fc29b1a4ceea4e9f2fd5bfe11a044d9
Sumaiya-topu <topusumaiya@gmail.com>
2023-12-13 21:05:42 +0600

    Personalised workout section card okay

--- public/js/script.js
+++ public/js/script.js
@@ -1,38 +1,8 @@
-// const uploadImage = () => {
-//   const input = document.getElementById("imageInput");
-//   const file = input.files[0];
-
-//   if (!file) {
-//     alert("Please select an image file.");
-//     return;
-//   }
-
-//   const formData = new FormData();
-//   formData.append("upload_images", file);
-
-//   fetch("http://localhost:5000/api/v1/upload/image-upload", {
-//     method: "POST",
-//     body: formData,
-//   })
-//     .then((response) => {
-//       if (!response.ok) {
-//         throw new Error("Image upload failed.");
-//       }
-//       return response.json();
-//     })
-//     .then((data) => {
-//       alert("Image uploaded successfully.");
-//     })
-//     .catch((error) => {
-//       console.error("Error:", error.message);
-//     });
-// };
-
 // Your existing uploadImage function
 const uploadImage = () => {
   const input = document.getElementById("imageInput");
   const file = input.files[0];
-
+  console.log("i am here");
   if (!file) {
     alert("Please select an image file.");
     return;
@@ -101,31 +71,6 @@ const dropArea = document.getElementById("dropArea");
 dropArea.addEventListener("dragover", handleDragOver);
 dropArea.addEventListener("dragleave", handleDragLeave);
 dropArea.addEventListener("drop", handleDrop);
-
-// Function to fetch content from Numbers API and update the page
-// const fetchNumbersApiContent = () => {
-//   $.ajax({
-//     url: "http://numbersapi.com/1/30/date?json",
-//     type: "GET",
-//     dataType: "json",
-//     success: function (data) {
-//       // Update the content on the page
-//       $("#numbersApiContent").html("<p>" + data.text + "</p>");
-//     },
-//     error: function (error) {
-//       console.error("Error fetching content from Numbers API", error);
-//       // Optionally, handle error display on the page
-//       $("#numbersApiContent").html(
-//         "<p>Error fetching content from Numbers API</p>"
-//       );
-//     },
-//   });
-// };
-
-// // Call the function when the page is loaded
-// $(document).ready(function () {
-//   fetchNumbersApiContent();
-// });
 
 // Function to fetch content from Numbers API and update the page
 function fetchNumbersApiContent() {
